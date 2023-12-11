fdc8935dfe2316a65200449056813b924037bc6a
Marco Ortiz <marcooolfoxygamer@gmail.com>
2023-12-11 17:24:35 -0500

    feat: Se agrega el formulario para crear registros de asistencias, así como se adelantan partes del funcionamiento del formulario de edición de anuncios

--- frontend/src/screens/Administrador/EdicionAnuncio.js
+++ frontend/src/screens/Administrador/EdicionAnuncio.js
@@ -1,14 +1,251 @@
-import React from 'react'
-import { SafeAreaView, View, Text, ImageBackground, Image, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState, useContext } from 'react'
+import { SafeAreaView, View, Text, ImageBackground, Image, Dimensions, StyleSheet, TextInput, ScrollView, Pressable, TouchableOpacity, Alert, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import axios from 'axios';
+import { BASE_URL } from '../../config';
+import { AuthContext } from '../../context/AuthContext';
+import * as FileSystem from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker';
 
 let deviceHeight = Dimensions.get('window').height;
 let deviceWidth = Dimensions.get('window').width;
 
 
-const EdicionAnuncio = ({navigation}) => {
+const EdicionAnuncio = ({navigation, route}) => {
+
+  let id_anunc = route.params?.id_anunc;
+
+  const {idUser} = useContext(AuthContext);
+
+  // Permisos
+  const [cameraPermissionInformation, requestPermission]=ImagePicker.useMediaLibraryPermissions();
+
+  // Campos a llenar
+  let [imagen, setImagen] = useState('');
+  let [titulo, setTitulo] = useState('');
+  let [descripcion, setDescripcion] = useState('');
+
+  // Campos originales de corroboración
+  let [idAdminAnuncOriginal, setIdAdminAnuncOriginal] = useState('');
+  let [imagenOriginal, setImagenOriginal] = useState('');
+  let [tituloOriginal, setTituloOriginal] = useState('');
+  let [descripcionOriginal, setDescripcionOriginal] = useState('');
+  let [estadoAnuncOriginal, setEstadoAnuncOriginal] = useState('');
+
+  // Errores
+  const [imagenError, setImagenError] = useState('');
+  const [tituloError, setTituloError] = useState('');
+  const [descripcionError, setDescripcionError] = useState('');
+
+  // Información subida correcta de la imagen
+  const [imagenCorrecta, setImagenCorrecta] = useState('')
+
+  // Validaciones
+  const validateImagen = () => {
+    if (!imagen) {
+      setImagenError('Por favor, incerte la imagen del anuncio');
+    } else {
+      setImagenError('');
+    }
+  };
+  const validateTitulo = () => {
+    if (!titulo) {
+      setTituloError('Por favor, indique el título que tendrá el anuncio');
+    } else {
+      setTituloError('');
+    }
+  };
+  const validateDescripcion = () => {
+    if (!descripcion) {
+      setDescripcionError('Por favor, indique la descripción que tendrá el anuncio');
+    } else {
+      setDescripcionError('');
+    }
+  };
+
+  // Gestion imagen
+
+  async function verifyPermission(){
+    if (cameraPermissionInformation.status===ImagePicker.PermissionStatus.UNDETERMINED){
+        const permissionResponse=await requestPermission();
+
+        return permissionResponse.granted;
+    }
+    if (cameraPermissionInformation.status===ImagePicker.PermissionStatus.DENIED){
+        Alert.alert(
+            'Permisos no concedidos!',
+            'Debe permitir acceder a los archivos del dispositivo para añadir anuncios'
+        );
+      return false
+    }
+    return true;
+  }
+  
+  const imagenGuardarHandler = async() => {
+    const hasPermission=await verifyPermission()
+    if (!hasPermission){
+        return;
+    }
+    try {
+      setImagenError('');
+      let image = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing:true,
+        aspect:[16,9],
+        quality:1
+      });
+      try {
+        setImagenError('');
+        let respuestaSubida = await FileSystem.uploadAsync(`${BASE_URL}/anuncios_subir_img`, image.assets[0].uri, {
+          httpMethod: 'POST',
+          uploadType: FileSystem.FileSystemUploadType.MULTIPART,
+          fieldName: 'file'
+        });
+        if (respuestaSubida.body == 'No hay archivos') {
+          setImagenError(respuestaSubida.body);
+        } else {
+          let nomb_img_con_comillas = respuestaSubida.body
+          let nomb_img = nomb_img_con_comillas.replace(/['"]+/g, '')
+          setImagen(nomb_img)
+          console.log(nomb_img);
+          setImagenCorrecta("Se ha subido la imagen correctamente")
+        }
+      }
+      catch {
+        setImagenError('Por favor, inserte la imagen del anuncio');
+      }
+    }
+    catch {
+      setImagenError('Por favor, inserte la imagen del anuncio');
+    }
+  }
+
+  // Submit
+  const handleSubmit = () => {
+    validateImagen();
+    validateTitulo();
+    validateDescripcion();
+  };
+
+  const handleCreacion = () => {
+    if (!imagenError && !tituloError && !descripcionError) {
+
+      if (idUser != idAdminAnuncOriginal) {
+        Alert.alert('Ups!', 'El anuncio que está editando no fue creado por usted. Por favor, contáctese con la persona que lo creó', [
+          {
+            text: 'Ok',
+            onPress: () => {
+              setTimeout(() => {
+                navigation.navigate('Listado de anuncios');
+              }, 100)
+            }
+          }
+        ])
+      } else {
+        editarAnuncio(idAdminAnuncOriginal, titulo, descripcion, imagen, estadoAnuncOriginal);
+      } 
+    }
+  };
+
+  const editarAnuncio = (fk_id_admin_anunc, titulo_anunc, desc_anunc, img_anunc, estado_anunc) => {
+
+    var config = {
+      method: 'get',
+      url: `${BASE_URL}/anuncios_imagenes/${img_anunc}`,
+    };
+    axios(config)
+    .then((response) => {
+      let resp = response.data;
+      if (resp == 'No hay registros') {
+        
+        // Actualizar el anuncio
+        var config_reg = {
+          method: 'post',
+          url: `${BASE_URL}/anuncios_agregar`,
+          data: {
+            fk_id_admin_anunc, titulo_anunc, desc_anunc, img_anunc, estado_anunc
+          }
+        };
+        axios(config_reg)
+        .then((response2) => {
+          let resp2 = response2.data;
+          if (resp2 == 'Se actualizó correctamente el anuncio') {
+            Alert.alert('¡Hecho!', 'Se ha actualizado correctamente el anuncio', [
+              {
+                text: 'Ok',
+                onPress: () => {
+                  setTimeout(() => {
+                    navigation.navigate('Listado de anuncios');
+                  }, 100)
+                },
+              }
+            ])
+          } else {
+            Alert.alert('Ups!', resp2, [
+              {
+                text: 'Ok'
+              }
+            ])
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        
+
+      } else {
+        setImagenError('Por favor, cambie el nombre de la imagen del anuncio ya que tenemos una con el mismo nombre');
+        // Alert.alert('Ups!', resp, [
+        //   {
+        //     text: 'Ok'
+        //   }
+        // ])
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  useEffect(() => {
+
+    // Obtencion datos del anuncio pasado por parámetro
+
+    var config = {
+      method: 'get',
+      url: `${BASE_URL}/anuncios_listado/${id_anunc}`,
+    };
+
+    axios(config)
+    .then(function (response) {
+      // console.log(JSON.stringify(response.data));
+
+      let campo_resp = response.data[0];
+      // setIdInstrucAsis(campo_resp.fk_id_admin_anunc);
+      setIdAdminAnuncOriginal(campo_resp.fk_id_admin_anunc);
+
+      
+
+      setImagen(campo_resp.img_anunc);
+      setImagenOriginal(campo_resp.img_anunc);
+
+      setTitulo(campo_resp.titulo_anunc);
+      setTituloOriginal(campo_resp.titulo_anunc);
+
+      setDescripcion(campo_resp.desc_anunc);
+      setDescripcionOriginal(campo_resp.desc_anunc);
+
+      
+
+      setEstadoAnuncOriginal(campo_resp.estado_anunc);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }, []);
+
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex:1}}>
         {/* Barra de navegación */}
         <View style={styles.container}>
           <TouchableOpacity onPress={()=>{
@@ -28,8 +265,74 @@ const EdicionAnuncio = ({navigation}) => {
         </View>
 
         {/* Vista */}
-        <View>
-        <Text>EdicionAnuncio</Text>
+        <View style={[styles.container_vista]}>
+          <View style={styles.contenedor_encabezado}>
+            <Text style={styles.titulo}>Edición de anuncio</Text>
+            <Text style={styles.subtitulo}>Edita los datos del anuncio</Text>
+            <View style={styles.lineaTexto}/>
+          </View>
+          <ScrollView>
+            <View style={styles.cont_1}>
+              <View style={styles.campo}>
+                {/* <TextInput
+                  style={styles.input}
+                  placeholder='Imagen'
+                  placeholderTextColor={'#666'}
+                  value={imagen}
+                  onChangeText={setImagen}
+                  onBlur={validateImagen}
+                /> */}
+                <Pressable
+                  style={styles.input}
+                  onPress={() => imagenGuardarHandler()}
+                >
+                  <Text style={styles.textInp}>Imagen del anuncio</Text>
+                </Pressable>
+                { imagenError ? <View style={styles.error}>
+                    <Text style={styles.errorText}>{imagenError}</Text>
+                </View> : null }
+                { imagenCorrecta ? <View style={styles.correct}>
+                    <Text style={styles.correctText}>{imagenCorrecta}</Text>
+                </View> : null }
+              </View>
+              <View style={styles.campo}>
+                <TextInput
+                  style={styles.input}
+                  placeholder='Titulo del anuncio'
+                  placeholderTextColor={'#666'}
+                  value={titulo}
+                  onChangeText={setTitulo}
+                  onBlur={validateTitulo}
+                />
+                { tituloError ? <View style={styles.error}>
+                    <Text style={styles.errorText}>{tituloError}</Text>
+                </View> : null }
+              </View>
+              <View style={[styles.campo, {height: 90}]}>
+                <TextInput
+                  style={[styles.input, styles.descInput]}
+                  placeholder='Descripción del anuncio'
+                  placeholderTextColor={'#666'}
+                  value={descripcion}
+                  onChangeText={setDescripcion}
+                  onBlur={validateDescripcion}
+                />
+                { descripcionError ? <View style={styles.error}>
+                    <Text style={styles.errorText}>{descripcionError}</Text>
+                </View> : null }
+              </View>
+            </View>
+            <View style={styles.separator} />
+            <View style={styles.cont_2}>
+              <Pressable
+                style={styles.btnEditar}
+                onPressIn={handleSubmit}
+                onPressOut={handleCreacion}
+              >
+                <Text style={styles.TextEditar}>Actualizar</Text>
+              </Pressable>
+            </View>
+          </ScrollView>
         </View>
     </SafeAreaView>
   )
@@ -44,10 +347,10 @@ const styles = StyleSheet.create({
       padding: 20,
     },
     container_vista: {
-      // justifyContent: 'center',
       flex: 1,
-      paddingTop: 20,
-      paddingHorizontal: 30,
+      backgroundColor: '#ffffffee',
+      // paddingTop: 20,s
+      paddingHorizontal: 20,
     },
     cont_1 : {
       flexGrow: 1,
@@ -58,75 +361,92 @@ const styles = StyleSheet.create({
       justifyContent: 'flex-start',
     },
     contenedor_encabezado: {
-      paddingHorizontal: 10,
+      paddingHorizontal: 20,
       paddingVertical: 20,
-      marginBottom: 40,
+      // marginBottom: 10,
     },
-    title: {
+    titulo: {
       fontFamily:'sans-serif',
       fontSize: 26,
       textAlign: 'center',
       fontWeight: '800',
       marginVertical: 18,
     },
-    subtitle: {
+    subtitulo: {
       fontFamily:'sans-serif',
-      fontSize: 15,
+      fontSize: 14,
       textAlign: 'center',
-      lineHeight: 23,
+      lineHeight: 20,
+      marginBottom: 5,
     },
     lineaTexto: {
       marginTop: 15,
       alignSelf: 'center',
-      // paddingHorizontal: 50,
       width: 50,
       height: 2,
       backgroundColor: '#7ED321'
     },
-    input: {
-      height: 45,
-      fontSize: 15,
-      borderColor: '#e2e2e2',
-      // borderStyle: 0,
-      backgroundColor: '#e2e2e2',
-      borderWidth: 1,
-      borderRadius: 10,
-      marginTop: 20,
-      paddingHorizontal: 10,
-      paddingLeft: 20,
-    },
-    error: {
-      color: 'red',
-      padding: 10,
-    },
     separator: {
       height: 30,
     },
-    btnLogin: {
-      // marginVertical: 20,
-      alignSelf: 'center',
-      backgroundColor: '#7ED321',
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 10,
+    campo: {
+      // marginTop: 10,
+      height: 'auto',
+      // marginBottom: 20,
+      marginHorizontal: 8,
       marginBottom: 20,
-      width: deviceWidth-130,
+      // backgroundColor: 'red'
     },
-    btnRecupContra: {
+    input: {
+      height: 50,
+      borderColor: '#e2e2e2',
+      borderWidth: 0,
+      borderRadius: 10,
+      backgroundColor: '#e2e2e2',
+      fontSize: 13,
+      padding:15,
+      borderRadius: 10,
+    },
+    descInput: {
+      height: '100%',
+      // marginBottom: 80,
+    },
+    textInp: {
+      color: '#5a5a5a',
+      fontSize: 13,
+    },
+    btnEditar: {
       // marginVertical: 20,
-      backgroundColor: '#73c120',
+      backgroundColor: '#7ED321',
       alignSelf: 'center',
       paddingVertical: 10,
       paddingHorizontal: 20,
       borderRadius: 10,
-      width: deviceWidth-130,
+      // marginTop: 45,
+      width: deviceWidth-230,
     },
-    btnText: {
+    TextEditar: {
       textAlign: 'center',
       color: '#fff',
       fontWeight: '800',
       fontSize: 16,
-    }
+    },
+    error: {
+      paddingTop: 10,
+      marginLeft: 10,
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 15,
+    },
+    correct: {
+      paddingTop: 10,
+      marginLeft: 10
+    },
+    correctText: {
+      color: 'green',
+      fontSize: 15,
+    },
 });
   
 
--- frontend/src/screens/Instructor/AgregarAsistencia.js
+++ frontend/src/screens/Instructor/AgregarAsistencia.js
@@ -1,14 +1,165 @@
-import React from 'react'
-import { SafeAreaView, View, Text, ImageBackground, Image, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState, useContext } from 'react'
+import { SafeAreaView, View, Text, ImageBackground, Image, Dimensions, StyleSheet, TouchableOpacity, TextInput, Pressable, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import axios from 'axios';
+import { BASE_URL } from '../../config';
+import { AuthContext } from '../../context/AuthContext';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { format } from 'date-fns';
+
 
 let deviceHeight = Dimensions.get('window').height;
 let deviceWidth = Dimensions.get('window').width;
 
 
 const AgregarAsistencia = ({navigation}) => {
+
+  const {idUser} = useContext(AuthContext);
+
+  // Campos a llenar
+  let [idInstrucAsis, setIdInstrucAsis] = useState('');
+  let [idAprendAsis, setIdAprendAsis] = useState('');
+  let [fechaAsis, setFechaAsis] = useState('');
+  let [estadoAsis, setEstadoAsis] = useState('');
+
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+
+  // Errores
+  const [idInstrucAsisError, setIdInstrucAsisError] = useState('');
+  const [idAprendAsisError, setIdAprendAsisError] = useState('');
+  const [fechaAsisError, setFechaAsisError] = useState('');
+
+  // Cambios del picker para la fecha
+  const toggleDatePicker = () => {
+    setShowPicker(!showPicker);
+  }
+
+  const onChangePicker = ({ type }, selectedDate) => {
+    if (type == "set") {
+      const currentDate = selectedDate;
+      setDate(currentDate);
+
+      if (Platform.OS == 'android') {
+        toggleDatePicker();
+        setFechaAsis(format(currentDate, "yyyy-MM-dd", {wareOfUnicodeTokens: true}))
+        // setFechaAsis(currentDate.toDateString());
+      }
+    } else {
+      toggleDatePicker();
+    }
+  }
+
+  // Validaciones
+  const validateIdInstrucAsis = () => {
+    if (!idInstrucAsis) {
+      setIdInstrucAsisError('Por favor indique su número de documento');
+    } else if (!/^\d+$/.test(idInstrucAsis)) {
+      setIdInstrucAsisError('La identificación del instructor debe contener solo números.');
+    } else if (idUser != idInstrucAsis) {
+      setIdInstrucAsisError('La identificación digitada es incorrecta. Por favor, revise que sea su identificación');
+    } else {
+      setIdInstrucAsisError('');
+    }
+  };
+
+  const validateIdAprendAsis = () => {
+    if (!idAprendAsis) {
+      setIdAprendAsisError('Por favor, digite el número de documento del aprendiz.');
+    } else if (!/^\d+$/.test(idAprendAsis)) {
+      setIdAprendAsisError('La identificación del aprendiz debe contener solo números.');
+    } else {
+
+      var config = {
+        method: 'post',
+        url: `${BASE_URL}/asistencia_listado_aprend`,
+        data: {
+          fk_id_aprend_asis: idAprendAsis
+        }
+      };
+
+      axios(config)
+      .then(function (response) {
+        if (response.data == 'Si existe') {
+            setIdAprendAsisError('');
+        }
+        else {
+          setIdAprendAsisError('La identificación digitada no existe en el sistema. Por favor, revise que la haya escrito correctamente');
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    }
+  };
+
+  const validateFechaAsis = () => {
+    if (!fechaAsis) {
+      setFechaAsisError('La fecha es obligatoria.');
+      // let fecha_act = new Date();
+      // let fecha = format(fecha_act, "yyyy-MM-dd", {wareOfUnicodeTokens: true})
+      // setFechaAsis(fecha);
+      // setDate(fecha);
+    } else {
+      setFechaAsisError('');
+    }
+  };
+
+  const handleSubmit = () => {
+    validateIdInstrucAsis();
+    validateIdAprendAsis();
+    validateFechaAsis();
+  };
+
+
+  const handleAccessList = () => {
+    if (!idInstrucAsisError && !idAprendAsisError && !fechaAsisError) {
+      agregar(idInstrucAsis, idAprendAsis, fechaAsis, estadoAsis);
+    }
+  };
+
+  const agregar = (id_instruc_asis, fk_id_aprend_asis, fecha_asis) => {
+    
+    axios.post(`${BASE_URL}/asistencia_agregar`, {
+      id_instruc_asis, fk_id_aprend_asis, fecha_asis
+    })
+    .then(response => {
+      let resp = response.data;
+      if (resp == 'Se agregó correctamente el registro') {
+        Alert.alert('¡Hecho!', 'Se ha creado correctamente el registro de asistencia', [
+          {
+            text: 'Ok',
+            onPress: () => {
+              setTimeout(() => {
+                navigation.navigate("Listado asistencias")
+              }, 100)
+            },
+          }
+        ])
+      } else {
+        Alert.alert('Ups!', resp, [
+          {
+            text: 'Ok'
+          }
+        ])
+      }   
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  useEffect(() => {
+    let fecha_act = new Date();
+    let fecha = format(fecha_act, "yyyy-MM-dd", {wareOfUnicodeTokens: true})
+    setFechaAsis(fecha);
+    setDate(new Date(fecha));
+  }, []);
+
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex: 1}}>
         {/* Barra de navegación */}
         <View style={styles.container}>
           <TouchableOpacity onPress={()=>{
@@ -28,8 +179,87 @@ const AgregarAsistencia = ({navigation}) => {
         </View>
 
         {/* Vista */}
-        <View>
-        <Text>AgregarAsistencia</Text>
+        <View style={[styles.container_vista]}>
+          <View style={styles.contenedor_encabezado}>
+            <Text style={styles.title}>Toma de asistencia</Text>
+            <Text style={styles.subtitle}>En este espacio, podrá tomar la asistencia al gimnasio por parte de los aprendices.</Text>
+            <View style={styles.lineaTexto}/>
+          </View>
+
+          {/* <ScrollView> */}
+            <View style={styles.cont_1}>
+              <View style={styles.campo}>
+                <TextInput
+                    style={[styles.input, styles.textInp]}
+                    placeholder='Identificación del Instructor'
+                    placeholderTextColor={'#666'}
+                    keyboardType='number-pad'
+                    value={idInstrucAsis.toString()}
+                    onChangeText={setIdInstrucAsis}
+                    onBlur={validateIdInstrucAsis}
+                    maxLength={15}
+                />
+                { idInstrucAsisError ? <View style={styles.error}>
+                    <Text style={styles.errorText}>{idInstrucAsisError}</Text>
+                </View> : null }
+              </View>
+              <View style={styles.campo}>
+                <TextInput
+                    style={[styles.input, styles.textInp]}
+                    placeholder='Identificación del Aprendiz'
+                    placeholderTextColor={'#666'}
+                    keyboardType='number-pad'
+                    value={idAprendAsis.toString()}
+                    onChangeText={setIdAprendAsis}
+                    onBlur={validateIdAprendAsis}
+                    maxLength={15}
+                />
+                { idAprendAsisError ? <View style={styles.error}>
+                    <Text style={styles.errorText}>{idAprendAsisError}</Text>
+                </View> : null }
+              </View>
+              <View style={styles.campo}>
+                { showPicker && (
+                  <DateTimePicker
+                    mode='date'
+                    display='spinner'
+                    value={date}
+                    onChange={onChangePicker}
+                  />
+                )}
+                {/* { !showPicker && ( */}
+                  < Pressable
+                    onPress={toggleDatePicker}
+                  >
+                    <TextInput
+                      style={[styles.input, styles.textInp]}
+                      placeholder='Fecha'
+                      placeholderTextColor={'#b2b2b2'}
+                      value={fechaAsis}
+                      onChangeText={setFechaAsis}
+                      onBlur={validateFechaAsis}
+                      editable={false}
+                      // icon={Ionicons}
+                    />
+                  </Pressable>
+                {/* )} */}
+
+                { fechaAsisError ? <View style={styles.error}>
+                    <Text style={styles.errorText}>{fechaAsisError}</Text>
+                </View> : null }
+              </View>
+            </View>
+            <View style={styles.separator} />
+            <View style={styles.cont_2}>
+              <Pressable
+                style={styles.btnGuardar}
+                onPressIn={handleSubmit}
+                onPressOut={handleAccessList}
+              >
+                <Text style={styles.TextGuardar}>Guardar</Text>
+              </Pressable>
+            </View>
+          {/* </ScrollView> */}
         </View>
     </SafeAreaView>
   )
@@ -44,10 +274,10 @@ const styles = StyleSheet.create({
       padding: 20,
     },
     container_vista: {
-      // justifyContent: 'center',
       flex: 1,
-      paddingTop: 20,
-      paddingHorizontal: 30,
+      backgroundColor: '#ffffffee',
+      // paddingTop: 20,s
+      paddingHorizontal: 20,
     },
     cont_1 : {
       flexGrow: 1,
@@ -60,7 +290,8 @@ const styles = StyleSheet.create({
     contenedor_encabezado: {
       paddingHorizontal: 10,
       paddingVertical: 20,
-      marginBottom: 40,
+      // marginBottom: 40,
+      marginBottom: 10,
     },
     title: {
       fontFamily:'sans-serif',
@@ -74,6 +305,7 @@ const styles = StyleSheet.create({
       fontSize: 15,
       textAlign: 'center',
       lineHeight: 23,
+      marginBottom: 5,
     },
     lineaTexto: {
       marginTop: 15,
@@ -83,49 +315,98 @@ const styles = StyleSheet.create({
       height: 2,
       backgroundColor: '#7ED321'
     },
-    input: {
-      height: 45,
-      fontSize: 15,
-      borderColor: '#e2e2e2',
-      // borderStyle: 0,
-      backgroundColor: '#e2e2e2',
-      borderWidth: 1,
-      borderRadius: 10,
-      marginTop: 20,
-      paddingHorizontal: 10,
-      paddingLeft: 20,
-    },
-    error: {
-      color: 'red',
-      padding: 10,
-    },
     separator: {
       height: 30,
     },
-    btnLogin: {
-      // marginVertical: 20,
-      alignSelf: 'center',
-      backgroundColor: '#7ED321',
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 10,
+    campo: {
+      // marginTop: 10,
+      height: 'auto',
+      // marginBottom: 20,
+      marginHorizontal: 8,
       marginBottom: 20,
-      width: deviceWidth-130,
+      // backgroundColor: 'red'
     },
-    btnRecupContra: {
+    input: {
+      height: 50,
+      borderColor: '#e2e2e2',
+      borderWidth: 0,
+      borderRadius: 10,
+      backgroundColor: '#e2e2e2',
+      fontSize: 13,
+      padding:15,
+      borderRadius: 10,
+    },
+    textInp: {
+      color: '#5a5a5a',
+    },
+    dropdown: {
+      height: 50,
+      borderColor: '#e2e2e2',
+      borderWidth: 0,
+      borderRadius: 10,
+      backgroundColor: '#e2e2e2',
+      paddingHorizontal: 8,
+    },
+    icon: {
+      marginRight: 5,
+    },
+    label: {
+      position: 'absolute',
+      backgroundColor: 'white',
+      left: 22,
+      top: 8,
+      zIndex: 999,
+      paddingHorizontal: 8,
+      fontSize: 13,
+    },
+    placeholderStyle: {
+      fontSize: 13,
+      paddingLeft:8,
+      color: 'rgb(115, 115, 115)',
+    },
+    selectedTextStyle: {
+      fontSize: 13,
+      paddingLeft:8,
+      color: 'rgb(115, 115, 115)',
+    },
+    iconStyle: {
+      width: 20,
+      height: 20,
+    },
+    inputSearchStyle: {
+      height: 40,
+      fontSize: 13,
+      // paddingLeft:15,
+    },
+    condicion1Input: {
+      height: 100,
+      // marginBottom: 80,
+    },
+  
+    btnGuardar: {
       // marginVertical: 20,
-      backgroundColor: '#73c120',
+      backgroundColor: '#7ED321',
       alignSelf: 'center',
       paddingVertical: 10,
       paddingHorizontal: 20,
       borderRadius: 10,
+      // marginTop: 45,
       width: deviceWidth-130,
     },
-    btnText: {
+    
+    TextGuardar: {
       textAlign: 'center',
       color: '#fff',
       fontWeight: '800',
       fontSize: 16,
+    },
+    error: {
+      paddingTop: 10,
+      marginLeft: 10,
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 15,
     }
 });
   
